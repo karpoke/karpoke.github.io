3acf6490bbfeadfeb32419a926ce7c17b06c88f0
karpoke <karpoke+github@gmail.com>
2019-06-18 22:35:11 +0200

    Added some links

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Ignacio Cano'
-SITENAME = u'Karpoke'
-SITEURL = ''
+AUTHOR = u"Ignacio Cano"
+SITENAME = u"Karpoke"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Madrid'
+TIMEZONE = "Europe/Madrid"
 
-DEFAULT_LANG = u'es'
+DEFAULT_LANG = u"es"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,14 +20,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Ignacio Cano", "https://www.ignaciocano.com"),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("atom", "{}/{}".format(SITEURL, FEED_ALL_ATOM)),
+)
 
 DEFAULT_PAGINATION = 10
 
@@ -35,12 +35,12 @@ DEFAULT_PAGINATION = 10
 RELATIVE_URLS = True
 
 # to use a custom domain in github pages
-STATIC_PATHS = ['extra/CNAME']
+STATIC_PATHS = ["extra/CNAME"]
 EXTRA_PATH_METADATA = {
-    'extra/CNAME': {'path': 'CNAME'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/humans.txt': {'path': 'humans.txt'},
-    'extra/robots.txt': {'path': 'robots.txt'},
+    "extra/CNAME": {"path": "CNAME"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/humans.txt": {"path": "humans.txt"},
+    "extra/robots.txt": {"path": "robots.txt"},
 }
 
 # disable cache when experimenting with different settings
