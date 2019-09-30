dcf2183c8869092f7ea50051eb2e4b2c5d3880c7
karpoke <karpoke+github@gmail.com>
2019-09-30 23:41:31 +0200

    Added pelican-plugins fork submodule

---
+++ sync-pelican-plugins.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# https://help.github.com/en/articles/fork-a-repo
+# https://help.github.com/en/articles/syncing-a-fork
+
+# add:		git remote add upstream https://github.com/getpelican/pelican-plugins.git
+# fetch:	git fetch upstream
+# checkout:	git checkout master
+# merge:	git merge upstream/master
---
+++ update-pelican-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# add: 		git submodule add https://github.com/karpoke/pelican-plugins pelican-plugins
+# install: 	git submodule update --init --recursive
+# updated: 	git submodule update --remote --merge --recursive
+git submodule update --remote --merge --recursive
