9f188973f376a64d1a3c4a8c18310ec32e6278c4
karpoke <karpoke+github@gmail.com>
2019-11-21 01:26:47 +0100

    Update requirements.txt after updating dependencies

--- upgrade-pelican.sh
+++ upgrade-pelican.sh
@@ -4,6 +4,8 @@ BASIC_PACKAGES="pelican markdown ghp-import typogrify beautifulsoup4 css-html-js
 
 if command -v pipenv > /dev/null; then
     pipenv update
+    pipenv run pip freeze | grep -v "pkg-resources" > requirements.txt
 else
     pip install --upgrade "$BASIC_PACKAGES"
+    pip freeze | grep -v "pkg-resources" > requirements.txt
 fi
