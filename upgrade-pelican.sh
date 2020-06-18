4beb10fddbbd0541ba87eb14df8707622f2c159f
karpoke <karpoke+github@gmail.com>
2020-06-18 14:32:59 +0200

    Updated dependencies

--- upgrade-pelican.sh
+++ upgrade-pelican.sh
@@ -1,11 +1,22 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-BASIC_PACKAGES="pelican markdown ghp-import typogrify beautifulsoup4 css-html-js-minify"
+BASIC_PACKAGES=(
+    pelican
+    markdown
+    ghp-import
+    typogrify
+    beautifulsoup4
+    css-html-js-minify
+    pyembed
+    pyembed-markdown
+    ipython
+)
 
 if command -v pipenv > /dev/null; then
     pipenv update
     pipenv run pip freeze | grep -v "pkg-resources" > requirements.txt
 else
-    pip install --upgrade "$BASIC_PACKAGES"
+    # shellcheck disable=SC2086
+    pip install --upgrade ${BASIC_PACKAGES[*]}
     pip freeze | grep -v "pkg-resources" > requirements.txt
 fi
